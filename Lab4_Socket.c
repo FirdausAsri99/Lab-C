360b510fefb07f30b92dfbfc6ce21ce95f26cf7c
FirdausAsri99 <abgfir1999@gmail.com>
2020-12-07 12:48:14 +0000

    Lab 4

---
+++ Lab4_Socket.c
@@ -0,0 +1,15 @@
+struct sockaddr_in {
+     short      sin_family;
+     unsigned short      sin_port;
+     struct in_addr      sin_addr;
+     char                sin_zero[8];
+};
+
+struct in_addr {
+     unsigned long s_addr;
+};
+
+struct sockaddr {
+     unsigned short      sa_family;
+     char                sa_data[14];
+};
